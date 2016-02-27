7f58a290925e51abd4042cd77ee73b3194c9acb3
cpepin <pepin.cameron@gmail.com>
2016-02-27 15:44:14 -0500

    Added templates directory

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,6 @@ var generators = require('yeoman-generator');
 module.exports = generators.NamedBase.extend({
   constructor: function() {
     generators.NamedBase.apply(this, arguments);
-    this.log('name: ', this.name);
   },
   method1: function(){
     this.log('Hello, World!');
